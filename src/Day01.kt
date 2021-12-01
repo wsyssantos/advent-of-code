c715149c8a2aa763e8dd8b61cc70693148c159a8
Wesley Santos Miyaguti <cloudwsy@gmail.com>
2021-12-01 09:20:27 +0100

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,48 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun calculate(input: List<Int>): Int {
+        var sum = 0
+        input.reduce { acc, value ->
+            if (value > acc) sum++
+            value
+        }
+        return sum
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun calculatePartTwo(input: List<Int>): Int {
+        var sum = 0
+        input.mapIndexed { index, _ ->
+            val sublist = input.getValidSublist(index)
+            if (sublist.all { it > 0 }.not()) {
+                null
+            } else {
+                sublist.sum()
+            }
+        }.filterNotNull().reduce { acc, value ->
+            if (value > acc) sum++
+            value
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(calculate(testInput) == 7)
+    check(calculatePartTwo(testInput) == 5)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("Day01").map { it.toInt() }
+    println(calculate(input))
+    println(calculatePartTwo(input))
+}
+
+fun List<Int>.getValidSublist(index: Int) : List<Int> =
+    listOf(
+        this.getOrZero(index),
+        this.getOrZero(index + 1),
+        this.getOrZero(index + 2),
+    )
+
+fun List<Int>.getOrZero(index: Int) : Int {
+    return if (index <= lastIndex) {
+        this[index]
+    } else 0
 }
