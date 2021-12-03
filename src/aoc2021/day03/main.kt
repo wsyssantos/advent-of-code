17d3b85f0719eff119c5b142ede7659d193dcb13
Wesley Santos Miyaguti <cloudwsy@gmail.com>
2021-12-03 10:13:25 +0100

    day 3 part 2

--- src/aoc2021/day03/main.kt
+++ src/aoc2021/day03/main.kt
@@ -3,14 +3,13 @@ package aoc2021.day03
 import readInput
 
 fun main() {
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput(2021, 3,"Day03_test")
     check(calculatePartOne(testInput) == 198)
     check(calculatePartTwo(testInput) == 230)
 
     val input = readInput(2021, 3,"Day03")
     println(calculatePartOne(input))
-//    println(calculatePartTwo(input))
+    println(calculatePartTwo(input))
 }
 
 fun calculatePartOne(input: List<String>) : Int {
@@ -27,25 +26,47 @@ fun calculatePartOne(input: List<String>) : Int {
         epsilonRate += gamaRateCount.getKeyFrom(gamaRateCount.minOf { it.value ?: 0 })
         gamaRateCount = mutableMapOf(0 to 0, 1 to 0)
     }
-    return Integer.parseInt(gammaRate, 2) * Integer.parseInt(epsilonRate, 2)
+    return gammaRate.bitToInt() * epsilonRate.bitToInt()
 }
 
 fun calculatePartTwo(input: List<String>) : Int {
-    if (input.size != 1) {
-        var gamaRateCount = mutableMapOf<Int, Int?>(0 to 0, 1 to 0)
-        val childSize = input.first().length
-        (0 until childSize).onEach { childPos ->
-            input.forEach { child ->
-                val bit = child[childPos].toString().toInt()
-                gamaRateCount[bit] = gamaRateCount[bit]?.plus(1)
-            }
-        }
-    } else {
-
-    }
+    val oxygenRating = getLastRemainingItem(0, 1, input).first()
+    val co2Rating = getLastRemainingItem(0, 0, input).first()
+    return oxygenRating.bitToInt() * co2Rating.bitToInt()
 }
 
-private fun Map<Int, Int?>.getKeyFrom(target: Int) : String =
+fun getLastRemainingItem(index: Int, factor: Int, input: List<String>) : List<String> {
+    if (input.size > 1) {
+        val rateMap = mutableMapOf<Int, List<String>?>(0 to emptyList(), 1 to emptyList())
+        input.forEach { child ->
+            val bit = child[index].toString().toInt()
+            rateMap[bit] = rateMap[bit]?.plus(child)
+        }
+        val zeroList = rateMap[0] ?: emptyList()
+        val oneList = rateMap[1] ?: emptyList()
+        return getLastRemainingItem(index + 1, factor, defineNextListByFactor(factor, oneList, zeroList))
+    }
+    return input
+}
+
+private fun defineNextListByFactor(factor: Int, oneList: List<String>, zeroList: List<String>) =
+    if (factor == 1) {
+        if (zeroList.size > oneList.size) {
+            zeroList
+        } else {
+            oneList
+        }
+    } else {
+        if (zeroList.size <= oneList.size) {
+            zeroList
+        } else {
+            oneList
+        }
+    }
+
+private fun String.bitToInt() = Integer.parseInt(this, 2)
+
+private fun Map<Int, Any?>.getKeyFrom(target: Int) : String =
     this.filter { map ->
         map.value == target
     }.keys.first().toString()
