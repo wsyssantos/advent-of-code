e7fde0dab50bc1d2cb20897b7e67711a6826d77c
Wesley Santos Miyaguti <cloudwsy@gmail.com>
2021-12-06 12:07:57 +0100

    day 6

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,10 @@ repositories {
     mavenCentral()
 }
 
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC")
+}
+
 tasks {
     sourceSets {
         main {
---
+++ src/aoc2021/day06/main.kt
@@ -0,0 +1,70 @@
+package aoc2021.day06
+
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
+import readInput
+import java.util.stream.Stream
+import kotlin.math.max
+import kotlin.system.measureTimeMillis
+
+fun main() = runBlocking {
+    val testInput = readInput(2021, 6,"test").sanitize()
+//    check(calculatePart2(testInput.asSequence(), 18) == 26L)
+//    check(calculatePart2(testInput.asSequence(), 80) == 5934L)
+    check(calculatePart2(testInput.asSequence(), 256) == 26984457539)
+//    check(calculatePart2(testInput.asFlow(), 18) == 26L)
+//    check(calculatePart2(testInput, 256) == 26984457539)
+
+//    launch(Dispatchers.Default) {
+//        check(calculatePart2(testInput.asSequence(), 1, 256) == 26984457539)
+//    }
+//    val input = readInput(2021, 6,"game").sanitize()
+//    print(calculatePart1(input, 80))
+}
+
+private fun calculatePart1(list: List<Int>, days: Int) : Int =
+    list.toMutableList().apply {
+        (1..days).forEach { _ ->
+            val newFishCount = this.filter { it == 0 }.size
+            this.forEachIndexed { index, i ->
+                val newValue = i - 1
+                this[index] = if (newValue >= 0) newValue else 6
+            }
+            (0 until newFishCount).forEach { _ ->
+                this.add(8)
+            }
+        }
+    }.size
+
+private suspend fun calculatePart2(list: Sequence<Int>, days: Int) : Long {
+    var sum = 0L
+    list.forEach {
+        val chunk = GlobalScope.async { calculateSequence(sequenceOf(it), maxDays = days) }
+        sum += chunk.await()
+    }
+    return sum
+}
+
+private tailrec suspend fun calculateSequence(list: Sequence<Int>, day: Int = 1, maxDays: Int) : Long {
+    if (day <= maxDays) {
+        val newFishCount = list.filter { it == 0 }.count()
+        list.map {
+            val newValue = it - 1
+            if (newValue >= 0) newValue else 6
+        }.let { newSequence ->
+            val newStream = sequence {
+                yieldAll(newSequence)
+                (0 until newFishCount).forEach { _ ->
+                    yield(8)
+                }
+            }
+            return calculateSequence(newStream, day + 1, maxDays)
+        }
+    } else {
+        return list.count().toLong()
+    }
+}
+
+
+private fun List<String>.sanitize() : List<Int> =
+    this.first().split(",").map { it.toInt() }
