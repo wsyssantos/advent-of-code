9f24d338753981f4ba11fb0c4d78c72b41920448
Wesley Santos Miyaguti <cloudwsy@gmail.com>
2021-12-06 18:16:45 +0100

    day 6

--- src/aoc2021/day06/main.kt
+++ src/aoc2021/day06/main.kt
@@ -1,32 +1,23 @@
 package aoc2021.day06
 
-import kotlinx.coroutines.*
 import readInput
 import kotlin.system.measureTimeMillis
 
-fun main() = runBlocking {
+
+fun main() {
     val testInput = readInput(2021, 6,"test").sanitize()
-//    check(calculatePart2(testInput, 18) == 26L)
-//    check(calculatePart2(testInput, 80) == 5934L)
-//    check(calculatePart2(testInput, 256) == 26984457539)
-//    check(calculatePart2(testInput.asFlow(), 18) == 26L)
-//    check(calculatePart2(testInput, 256) == 26984457539)
-
-//    launch(Dispatchers.Default) {
-//        check(calculatePart2(testInput.asSequence(), 1, 256) == 26984457539)
-//    }
-    measureTimeMillis {
-        println(calculatePart2(listOf(1), 192))
-    }.also {
-        println("$it milliseconds")
-    }
-
-//    524288
-//    4058 milliseconds
+    check(calculatePart1(testInput, 18) == 26)
+    check(calculatePart1(testInput, 80) == 5934)
+    check(calculatePart2(testInput, 256) == 26984457539L)
 
     val input = readInput(2021, 6,"game").sanitize()
-//    print(calculatePart1(input, 80))
-//    print(calculatePart2(listOf(1), 256))
+    measureTimeMillis {
+        println(calculatePart1(input, 80))
+        println(calculatePart2(input, 256))
+    }.also {
+        println("$it milli")
+    }
+
 }
 
 private fun calculatePart1(list: List<Int>, days: Int) : Int =
@@ -43,26 +34,30 @@ private fun calculatePart1(list: List<Int>, days: Int) : Int =
         }
     }.size
 
-private suspend fun calculatePart2(list: List<Int>, days: Int) : Long =
-    list.map {
-        awaitAsync { calculateSequenceAsync(it, maxDays = days) }
-    }.awaitAll().sum()
-
-private tailrec suspend fun calculateSequenceAsync(number: Int, day: Int = 1, maxDays: Int) : Long = withContext(Dispatchers.Unconfined) {
-    if (day <= maxDays) {
-        return@withContext if (number > 0) {
-            calculateSequenceAsync(number - 1, day + 1, maxDays)
-        } else {
-            val first = calculateSequenceAsync(6, day + 1, maxDays)
-            val second = calculateSequenceAsync(6, day + 1, maxDays)
-            first + second
+private fun calculatePart2(list: List<Int>, days: Int) : Long {
+    var agingGroup = list.groupBy { it }.mapValues { (_, value) ->
+        value.size.toLong()
+    }
+    (1..days).forEach { _ ->
+        val iterator = agingGroup.toSortedMap(Comparator.reverseOrder()).iterator()
+        agingGroup = mutableMapOf<Int, Long>().apply {
+            iterator.forEach { (key, value) ->
+                if (key == 0) {
+                    val nextValue = getOrDefault(8, 0)
+                    remove(8)
+                    putIfAbsent(8, nextValue + value)
+                    val renewValue = getOrDefault(6, 0)
+                    remove(6)
+                    putIfAbsent(6, renewValue + value)
+                } else {
+                    val nextKey = key - 1
+                    putIfAbsent(nextKey, value)
+                }
+            }
         }
     }
-    return@withContext 1L
+    return agingGroup.values.sumOf { it }
 }
-
-private fun <T> awaitAsync(block: suspend CoroutineScope.() -> T) : Deferred<T> =
-    GlobalScope.async(Dispatchers.Unconfined, block = block)
 
 private fun List<String>.sanitize() : List<Int> =
     this.first().split(",").map { it.toInt() }
